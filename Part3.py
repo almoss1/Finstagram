d8a97138f0733611fe7c16bbd94da9870896bfa8
Alex Mosseri <aljmo98@gmail.com>
2019-11-21 16:00:35 -0400

    Start to project

---
+++ Part3.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+# cnx = mysql.connector.connect(
+#     user='alex', 
+#     passwd='123456',
+#     host='localhost'
+# )
+# cnx.close()
+
+
+# import pyodbc 
+# conn = pyodbc.connect('Driver={SQL Server};'
+#                       'Server=localhost:8889;'
+#                       'Database=University;'
+#                       'Trusted_Connection=yes;')
+
+# cursor = conn.cursor()
+# cursor.execute('SELECT * FROM advisor')
+
+# for row in cursor:
+#     print(row)
+
+# query = ("SELECT * FROM advisor")
+
+# print(query)
