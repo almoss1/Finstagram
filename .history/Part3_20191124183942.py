518160a7ee5ace89d4893ad826e6648dc093cd20
Jonathan Wiener <jiw232@gmail.com>
2019-11-24 20:33:21 -0500

    feature 3 done

--- .history/Part3_20191124183942.py
+++ .history/Part3_20191124183942.py
@@ -1,7 +1,6 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
-import datetime
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -11,8 +10,8 @@ conn = pymysql.connect(host='localhost',
                        port = 8889,
                        user='root',
                        password='root',
-                       db='finstagram',
-                       charset='utf8mb4',
+                       db='Finstagram',
+                       charset='uztf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
 
 #Define a route to hello function
@@ -29,17 +28,6 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
-
-@app.route('/home')
-def home():
-    user = session['username']
-    cursor = conn.cursor();
-    query = 'SELECT photoID,photoPoster FROM Photo WHERE photoPoster = %s' # note: only checks for when user is poster
-    cursor.execute(query, (user))
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('home.html', username=user, posts=data)
-
 
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
@@ -74,9 +62,6 @@ def registerAuth():
     #grabs information from the forms
     username = request.form['username']
     password = request.form['password']
-    firstName = request.form['firstName']
-    lastName = request.form['lastName']
-    bio = request.form['bio']
 
     #cursor used to send queries
     cursor = conn.cursor()
@@ -92,35 +77,38 @@ def registerAuth():
         error = "This user already exists"
         return render_template('register.html', error = error)
     else:
-        ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password, firstName, lastName, bio))
+        ins = 'INSERT INTO Person VALUES(%s, %s)'
+        cursor.execute(ins, (username, password))
         conn.commit()
         cursor.close()
         return render_template('index.html')
+
+
+# @app.route('/home')
+# def home():
+#     user = session['username']
+#     cursor = conn.cursor();
+#     query = 'SELECT username,password FROM Person'
+#     cursor.execute(query, (user))
+#     data = cursor.fetchall()
+#     cursor.close()
+#     return render_template('home.html', username=user, posts=data)
 
         
 
 
 
 
-@app.route('/post', methods=['GET', 'POST'])
-def post():
-    username = session['username']
-    cursor = conn.cursor();
-
-    filepath = request.form['filepath']
-    caption = request.form['caption']
-    isAllFollowers = request.form['isAllFollowers']
-    if (isAllFollowers == 'true'):
-        isAllFollowers = True
-    else:
-        isAllFollowers = False
-
-    query = 'INSERT INTO photo (filepath, caption, allFollowers, photoPoster, postingdate) VALUES(%s, %s, %s, %s, %s)'
-    cursor.execute(query, (filepath, caption, isAllFollowers, username, datetime.datetime.now()))
-    conn.commit()
-    cursor.close()
-    return redirect(url_for('home'))
+# @app.route('/post', methods=['GET', 'POST'])
+# def post():
+#     username = session['username']
+#     cursor = conn.cursor();
+#     blog = request.form['blog']
+#     query = 'INSERT INTO blog (blog_post, username) VALUES(%s, %s)'
+#     cursor.execute(query, (blog, username))
+#     conn.commit()
+#     cursor.close()
+#     return redirect(url_for('home'))
 
 # @app.route('/select_blogger')
 # def select_blogger():
