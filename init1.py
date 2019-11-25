b5881a4e4a131275cfde5187b79f10042673a640
Alex Mosseri <aljmo98@gmail.com>
2019-11-25 19:19:49 -0400

    not finsihed search poster

--- init1.py
+++ init1.py
@@ -32,90 +32,20 @@ def register():
 
 @app.route('/home')
 def home():
-    ##NEED HELP WITH OUPUTTING THIS QUERY
+
     user = session['username']
     cursor = conn.cursor();
-    # query = 'SELECT photoID,photoPoster,caption  FROM Photo   WHERE photoPoster = %s'
 
-# RIGHT ONE
+
     query = 'SELECT DISTINCT photoID, photoPoster FROM Photo WHERE photoPoster = %s OR (photoID, photoPoster) IN(SELECT photoID, photoPoster FROM (Photo AS P JOIN Follow AS F ON (F.username_followed=P.photoPoster)) WHERE followstatus=TRUE AND P.allFollowers=True AND F.username_follower = %s)OR (photoID, photoPoster) IN (SELECT photoID, photoPoster FROM SharedWith JOIN BelongTo ON (SharedWith.groupOwner= BelongTo.owner_username AND SharedWith.groupName=BelongTo.groupName) WHERE SharedWith.photoID = photoID AND BelongTo.member_username = %s)'
 
-# follow -- username_followed === photoposter
-    #ownwer of phot = user
 
-    # user = belong to frinedgroup that shared with
-    # user follows the owner of phot and photo allfolllowers == true
   
 
     cursor.execute(query, (user, user, user))
     data = cursor.fetchall()
     cursor.close()
     return render_template('home.html', username=user, posts=data)
-
-# @app.route('/home')
-# def visible_photo():
-
-
-#Authenticates the login
-@app.route('/loginAuth', methods=['GET', 'POST'])
-def loginAuth():
-    #grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-
-    #cursor used to send queries
-    cursor = conn.cursor()
-    #executes query
-    query = 'SELECT * FROM Person WHERE username = %s and password = %s'
-    cursor.execute(query, (username, password))
-    #stores the results in a variable
-    data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
-    cursor.close()
-    error = None
-    if(data):
-        #creates a session for the the user
-        #session is a built in
-        session['username'] = username
-        return redirect(url_for('home'))
-    else:
-        #returns an error message to the html page
-        error = 'Invalid login or username'
-        return render_template('login.html', error=error)
-
-#Authenticates the register
-@app.route('/registerAuth', methods=['GET', 'POST'])
-def registerAuth():
-    #grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-    firstName = request.form['firstName']
-    lastName = request.form['lastName']
-    bio = request.form['bio']
-
-    #cursor used to send queries
-    cursor = conn.cursor()
-    #executes query
-    query = 'SELECT * FROM Person WHERE username = %s'
-    cursor.execute(query, (username))
-    #stores the results in a variable
-    data = cursor.fetchone()
-    #use fetchall() if you are expecting more than 1 data row
-    error = None
-    if(data):
-        #If the previous query returns data, then user exists
-        error = "This user already exists"
-        return render_template('register.html', error = error)
-    else:
-        ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s, %s)'
-        cursor.execute(ins, (username, password, firstName, lastName, bio))
-        conn.commit()
-        cursor.close()
-        return render_template('index.html')
-
-        
-
-
 
 
 @app.route('/post', methods=['GET', 'POST'])
@@ -210,6 +140,132 @@ def reject_follower(follower):
     cursor.close()
 
     return manage_follow_requests()
+
+
+@app.route('/show_photo/<int:currPhotoID>', methods=["GET"])
+def show_photo(currPhotoID):
+    cursor = conn.cursor()
+    # currPost = request.args['photoID']
+    query = 'SELECT * FROM photo JOIN Person ON(username=photoPoster)  WHERE photoID=%s'
+    cursor.execute(query, (currPhotoID))
+    data = cursor.fetchone()
+
+    query = 'SELECT * FROM Tagged NATURAL JOIN Person WHERE photoID=%s AND tagstatus=TRUE'
+    cursor.execute(query, (currPhotoID))
+    taggees = cursor.fetchone()
+
+    query = 'SELECT * FROM Likes NATURAL JOIN Person WHERE photoID=%s'
+    cursor.execute(query, (currPhotoID))
+    likes = cursor.fetchone()
+
+    cursor.close()
+    return render_template('show_photo.html', post=data, tagged=taggees, likees=likes)
+
+
+@app.route('/search_by_poster')
+def search_by_poster():
+    return render_template('search_by_poster.html')
+
+
+@app.route('/search', methods = ['GET','POST'])
+def search():
+    user = session['username']
+    cursor = conn.cursor();
+    photoPoster = request.form['photoPoster']
+
+    query = 'SELECT DISTINCT photoID, photoPoster FROM Photo WHERE photoPoster = %s OR (photoID, photoPoster) IN(SELECT photoID, photoPoster FROM (Photo AS P JOIN Follow AS F ON (F.username_followed=P.photoPoster)) WHERE followstatus=TRUE AND P.allFollowers=True AND F.username_follower = %s)OR (photoID, photoPoster) IN (SELECT photoID, photoPoster FROM SharedWith JOIN BelongTo ON (SharedWith.groupOwner= BelongTo.owner_username AND SharedWith.groupName=BelongTo.groupName) WHERE SharedWith.photoID = photoID AND BelongTo.member_username = %s)'
+
+    cursor.execute(query, (photoPoster, photoPoster, photoPoster))
+    data = cursor.fetchall()
+
+    if(data is None):
+        error = "Invalid Username"
+        return render_template('search_by_poster.html', error=error)
+
+
+
+    cursor.close()
+    return render_template('home.html', username=user, posts=data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Authenticates the login
+@app.route('/loginAuth', methods=['GET', 'POST'])
+def loginAuth():
+    #grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM Person WHERE username = %s and password = %s'
+    cursor.execute(query, (username, password))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if(data):
+        #creates a session for the the user
+        #session is a built in
+        session['username'] = username
+        return redirect(url_for('home'))
+    else:
+        #returns an error message to the html page
+        error = 'Invalid login or username'
+        return render_template('login.html', error=error)
+
+#Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+    #grabs information from the forms
+    username = request.form['username']
+    password = request.form['password']
+    firstName = request.form['firstName']
+    lastName = request.form['lastName']
+    bio = request.form['bio']
+
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM Person WHERE username = %s'
+    cursor.execute(query, (username))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    error = None
+    if(data):
+        #If the previous query returns data, then user exists
+        error = "This user already exists"
+        return render_template('register.html', error = error)
+    else:
+        ins = 'INSERT INTO Person VALUES(%s, %s, %s, %s, %s)'
+        cursor.execute(ins, (username, password, firstName, lastName, bio))
+        conn.commit()
+        cursor.close()
+        return render_template('index.html')
+
+        
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
+
+
+
+
 # @app.route('/select_blogger')
 # def select_blogger():
 #     #check that user is logged in
@@ -233,29 +289,6 @@ def reject_follower(follower):
 #     cursor.close()
 #     return render_template('show_posts.html', poster_name=poster, posts=data)
 
-@app.route('/show_photo/<int:currPhotoID>', methods=["GET"])
-def show_photo(currPhotoID):
-    cursor = conn.cursor()
-    # currPost = request.args['photoID']
-    query = 'SELECT * FROM photo JOIN Person ON(username=photoPoster)  WHERE photoID=%s'
-    cursor.execute(query, (currPhotoID))
-    data = cursor.fetchone()
-
-    query = 'SELECT * FROM Tagged NATURAL JOIN Person WHERE photoID=%s AND tagstatus=TRUE'
-    cursor.execute(query, (currPhotoID))
-    taggees = cursor.fetchone()
-
-    query = 'SELECT * FROM Likes NATURAL JOIN Person WHERE photoID=%s'
-    cursor.execute(query, (currPhotoID))
-    likes = cursor.fetchone()
-
-    cursor.close()
-    return render_template('show_photo.html', post=data, tagged=taggees, likees=likes)
-
-@app.route('/logout')
-def logout():
-    session.pop('username')
-    return redirect('/')
         
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
