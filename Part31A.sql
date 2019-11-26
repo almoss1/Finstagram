8a296950f42a43173e207cec8ded8eb4911e2ee1
Jonathan Wiener <jonathan.wiener98@gmail.com>
2019-11-26 11:34:51 -0500

    no message

--- Part31A.sql
+++ Part31A.sql
@@ -1,3 +1,14 @@
+SELECT PhotoID
+FROM Photo
+WHERE PhotoID IN 
+    (SELECT PhotoID
+    FROM (Photo JOIN Person ON (Photo.PhotoPoster=Person.Username)) JOIN Follow ON (Follow.username_followed=Photo.PhotoPoster)
+    WHERE followstatus=True AND username_follower="TestUser") OR PhotoID IN 
+        (SELECT PhotoID
+        FROM SharedWith JOIN BelongTo ON (SharedWith.groupOwner=BelongTo.owner_username AND SharedWith.groupName=BelongTo.groupName)
+        WHERE member_username="testuser");
+
+
 SELECT DISTINCT photoID, photoPoster 
 FROM Photo 
 WHERE photoPoster = "TestUser"
