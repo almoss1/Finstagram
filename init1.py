d23dec7f34391d11a0a5d270e3aa72c80fb23dfe
Alex Mosseri <aljmo98@gmail.com>
2019-12-02 21:03:02 -0400

    missed punctuation

--- init1.py
+++ init1.py
@@ -218,7 +218,7 @@ def search():
     photoPoster = request.form['photoPoster']
 
 ### need to make it that 
-    query = 'SELECT DISTINCT photoID, photoPoster FROM Photo WHERE photoPoster = %s 
+    query = 'SELECT DISTINCT photoID, photoPoster FROM Photo WHERE photoPoster = %s '
     # OR (photoID, photoPoster) IN(SELECT photoID, photoPoster FROM (Photo AS P JOIN Follow AS F ON (F.username_followed=P.photoPoster)) WHERE followstatus=TRUE AND P.allFollowers=True AND F.username_follower = %s)OR (photoID, photoPoster) IN (SELECT photoID, photoPoster FROM SharedWith JOIN BelongTo ON (SharedWith.groupOwner= BelongTo.owner_username AND SharedWith.groupName=BelongTo.groupName) WHERE SharedWith.photoID = photoID AND BelongTo.member_username = %s)'
     cursor.execute(query, (photoPoster))
     data = cursor.fetchall()
